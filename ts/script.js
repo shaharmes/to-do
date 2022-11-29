dff38a1fc22da19e07b937a1146c16accf51f5d0
Shahar <shaharmes33@gmail.com>
2022-11-29 05:36:50 +0200

    initial commit

---
+++ ts/script.js
@@ -0,0 +1,38 @@
+const list = document.querySelector('#list');
+const input = document.querySelector('#input');
+const dateElement = document.getElementById("date");
+const LIST = [];
+let id = 0;
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+// Show todays date
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+document.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const toDo = input.value;
+        if (toDo) {
+            addToDo(toDo, id, false, false);
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            });
+        }
+        input.value = '';
+    }
+});
+function addToDo(toDo, id, done, trash) {
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : '';
+    const text = '<li class="item">' +
+        '<i class="fa ' + DONE + ' co" job="complete" id="' + id + '"></i>' +
+        '<p class="text ' + LINE + '">' + toDo + '</p>' +
+        '<i class="fa fa-trash-o de" job="delete" id="' + id + '"></i>' +
+        '</li>';
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+}
---
+++ ts/script.ts
@@ -0,0 +1,49 @@
+const list : HTMLElement = document.querySelector('#list');
+const input : HTMLInputElement = document.querySelector('#input');
+const dateElement : HTMLElement = document.getElementById("date");
+const LIST : any[] = [];
+let id : number = 0;
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+
+// Show todays date
+const options : any = {weekday : "long", month : "short", day : "numeric"};
+const today : Date = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+
+
+
+document.addEventListener('keyup', function (event : KeyboardEvent) {
+    if (event.key === 'Enter') {
+        const toDo = input.value;
+        if (toDo) {
+            addToDo(toDo, id, false, false);
+            LIST.push(
+                {
+                    name: toDo,
+                    id: id,
+                    done: false,
+                    trash: false
+                });
+        }
+        input.value = '';
+    }
+});
+
+function addToDo (toDo : string, id : number, done : boolean, trash : boolean) {
+
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : '';
+
+    const text = '<li class="item">' +
+        '<i class="fa ' + DONE + ' co" job="complete" id="' + id + '"></i>' +
+        '<p class="text ' + LINE + '">' + toDo + '</p>' +
+        '<i class="fa fa-trash-o de" job="delete" id="' + id + '"></i>' +
+        '</li>';
+    const position = "beforeend";
+    list.insertAdjacentHTML(position, text);
+                    
+}
+
